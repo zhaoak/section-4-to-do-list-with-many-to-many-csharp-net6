17f843fd6ffa067507f95841e5fae71861024a28
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-31 14:33:50 -0700

    Add simple delete route and page for Tags

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -57,6 +57,21 @@ public class TagsController : Controller
     return RedirectToAction("Index");
   }
 
+  public ActionResult Delete(int id)
+  {
+    Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+    return View(thisTag);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+    _db.Tags.Remove(thisTag);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
   public ActionResult AddItem(int id)
   {
     Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
