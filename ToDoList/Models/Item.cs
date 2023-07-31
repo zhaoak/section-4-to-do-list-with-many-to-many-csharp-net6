3a9bfe4049a65d96056aeeb6b1e471c18ea775b4
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-31 15:34:07 -0700

    Add validation for item requiring description

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,6 +6,7 @@ namespace ToDoList.Models
   public class Item
   {
     public int ItemId { get; set; }
+    [Required(ErrorMessage = "Items must have a description.")]
     public string Description { get; set; }
     [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category before it can be added.")]
     public int CategoryId { get; set; }
